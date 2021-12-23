7bd8674c8a031ca4c69b9f4ee90e77eb181b5889
jarbus <jarbus@tutanota.com>
2021-12-22 20:56:54 -0500

    Reworked tests

--- test/epsilon-network.jl
+++ test/epsilon-network.jl
@@ -1,6 +1,12 @@
-@testset "EpsilonNetworks" begin
+@testset "basics" begin
     en = EpsilonNetwork(1)
     @test EpsilonNetworks.neurons(en) == [1]
     EpsilonNetworks.add_neuron!(en)
     @test EpsilonNetworks.neurons(en) == [1, 2]
+    EpsilonNetworks.activate_neuron!(en, 1)
+    @test 1 ∈ EpsilonNetworks.active_neurons(en)
+    @test 2 ∉ EpsilonNetworks.active_neurons(en)
+    EpsilonNetworks.deactivate_neuron!(en, 1)
+    @test 1 ∉ EpsilonNetworks.active_neurons(en)
+    @test 2 ∉ EpsilonNetworks.active_neurons(en)
 end
